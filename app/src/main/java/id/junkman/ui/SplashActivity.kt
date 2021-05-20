e1e41facb3ab12010f3bbc5ac8bfda31f7639ef4
angelanst <45375762+angelanst@users.noreply.github.com>
2021-05-20 13:31:33 +0700

    Update Splash Activity

--- app/src/main/java/id/junkman/ui/SplashActivity.kt
+++ app/src/main/java/id/junkman/ui/SplashActivity.kt
@@ -1,12 +1,20 @@
 package id.junkman.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import id.junkman.R
 
 class SplashActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
+
+    Handler(Looper.getMainLooper()).postDelayed({
+      startActivity(Intent(this@SplashActivity, LandingActivity::class.java))
+      finish()
+    }, 3000)
   }
 }
