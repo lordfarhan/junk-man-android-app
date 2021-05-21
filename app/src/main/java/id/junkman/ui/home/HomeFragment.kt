b1b6e4dbad27fbfe82b7a46ff28a3edcb7cdceee
angelanst <45375762+angelanst@users.noreply.github.com>
2021-05-21 21:02:41 +0700

    Initial Home Fragment

--- app/src/main/java/id/junkman/ui/home/HomeFragment.kt
+++ app/src/main/java/id/junkman/ui/home/HomeFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
 import id.junkman.R
+import id.junkman.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
@@ -15,12 +18,19 @@ class HomeFragment : Fragment() {
   }
 
   private lateinit var viewModel: HomeViewModel
+  private var _binding: FragmentHomeBinding? = null
+  private val binding get() = _binding!!
 
-  override fun onCreateView(
-    inflater: LayoutInflater, container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_home, container, false)
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+    val poster = ArrayList<SlideModel>()
+    poster.add(SlideModel(R.drawable.card1, getString(R.string.card1), ScaleTypes.CENTER_CROP))
+    poster.add(SlideModel(R.drawable.card2, getString(R.string.card2), ScaleTypes.CENTER_CROP))
+    poster.add(SlideModel(R.drawable.card3, getString(R.string.card3), ScaleTypes.CENTER_CROP))
+
+    binding.imageSlider.setImageList(poster)
+    return binding.root
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
