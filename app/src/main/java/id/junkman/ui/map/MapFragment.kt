42e9aba7723a4b391441940ddcf36b3f250efcfe
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2021-05-21 23:16:16 +0700

    Initial map

--- app/src/main/java/id/junkman/ui/map/MapFragment.kt
+++ app/src/main/java/id/junkman/ui/map/MapFragment.kt
@@ -1,32 +1,56 @@
 package id.junkman.ui.map
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import id.junkman.R
+import id.junkman.databinding.FragmentMapBinding
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
   companion object {
     fun newInstance() = MapFragment()
   }
 
+  private lateinit var binding: FragmentMapBinding
   private lateinit var viewModel: MapViewModel
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
-  ): View? {
-    return inflater.inflate(R.layout.fragment_map, container, false)
+  ): View {
+    binding = FragmentMapBinding.inflate(layoutInflater, container, false)
+    return binding.root
   }
 
-  override fun onActivityCreated(savedInstanceState: Bundle?) {
-    super.onActivityCreated(savedInstanceState)
-    viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
-    // TODO: Use the ViewModel
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    if (activity != null) {
+      viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+      // Get the SupportMapFragment and request notification when the map is ready to be used.
+      val mapFragment =
+        childFragmentManager.findFragmentById(R.id.fragment_map) as? SupportMapFragment
+      mapFragment?.getMapAsync(this)
+    }
   }
 
+  override fun onMapReady(googleMap: GoogleMap) {
+    val sydney = LatLng(-33.852, 151.211)
+    googleMap.addMarker(
+      MarkerOptions()
+        .position(sydney)
+        .title("Marker in Sydney")
+    )
+    googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f))
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+  }
 }
