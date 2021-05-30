54a51d7e9c9141d85faf7f7eff85136fc7c6743f
angelanst <45375762+angelanst@users.noreply.github.com>
2021-05-30 23:37:12 +0700

    Init Shop Fragment adding Search View (progress)

--- app/src/main/java/id/junkman/ui/shop/ShopAdapter.kt
+++ app/src/main/java/id/junkman/ui/shop/ShopAdapter.kt
@@ -2,25 +2,34 @@ package id.junkman.ui.shop
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import id.junkman.databinding.ItemProductBinding
 import id.junkman.model.Product
+import java.util.*
 
 /**
  * @author farhan
  * created at at 18:55 on 28/05/21.
  */
-class ShopAdapter : ListAdapter<Product, ShopAdapter.ViewHolder>(DiffCallback()) {
+class ShopAdapter : ListAdapter<Product, ShopAdapter.ViewHolder>(DiffCallback()), Filterable {
   var onItemClick: ((Product) -> Unit)? = null
+  private var list = mutableListOf<String>()
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
     ViewHolder(ItemProductBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
     holder.bind(getItem(position))
+  }
+
+  fun setData(listData: MutableList<String>){
+    this.list = listData
+    submitList(listData)
   }
 
   inner class ViewHolder(private val binding: ItemProductBinding) :
@@ -37,6 +46,59 @@ class ShopAdapter : ListAdapter<Product, ShopAdapter.ViewHolder>(DiffCallback())
       }
     }
   }
+
+  override fun getFilter(): Filter {
+    return customFilter
+  }
+
+  /*private val customFilter = object: Filter() {
+    override fun performFiltering(constraint: CharSequence?): FilterResults {
+      val filteredList = mutableListOf<Product>()
+      if (constraint == null || constraint.isEmpty()) {
+        filteredList.addAll(currentList)
+      } else {
+        val filterPattern = constraint.toString().trim { it <= ' ' }
+        for (item in currentList) {
+          if (item.name?.contains(filterPattern) == true) {
+            filteredList.add(item)
+          }
+        }
+      }
+      val results = FilterResults()
+      results.values = filteredList
+      return results
+    }
+
+    override fun publishResults(constraint: CharSequence?, filterResults: FilterResults?) {
+      list.clear()
+      list.addAll(filterResults?.values as MutableList<Product>)
+      submitList(list)
+//      notifyDataSetChanged()
+    }*/
+
+
+    private val customFilter = object : Filter() {
+      override fun performFiltering(constraint: CharSequence?): FilterResults {
+        val filteredList = mutableListOf<String>()
+        if (constraint == null || constraint.isEmpty()) {
+          filteredList.addAll(list)
+        } else {
+          for (item in list) {
+            if (item.startsWith(constraint.toString().toLowerCase())) {
+              filteredList.add(item)
+            }
+          }
+        }
+        val results = FilterResults()
+        results.values = filteredList
+        return results
+      }
+
+      override fun publishResults(constraint: CharSequence?, filterResults: FilterResults?) {
+        submitList(filterResults?.values as MutableList<String>)
+      }
+  }
+
 }
 
 class DiffCallback() : DiffUtil.ItemCallback<Product>() {
--- app/src/main/java/id/junkman/ui/shop/ShopFragment.kt
+++ app/src/main/java/id/junkman/ui/shop/ShopFragment.kt
@@ -2,17 +2,20 @@ package id.junkman.ui.shop
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import id.junkman.databinding.FragmentShopBinding
 import id.junkman.model.Product
-import id.junkman.ui.shoppingcart.ShoppingCartActivity
+import id.junkman.ui.shoppingcart.CartActivity
 import id.junkman.utils.gone
 import id.junkman.utils.visible
 
@@ -38,6 +41,8 @@ class ShopFragment : Fragment() {
     store = Firebase.firestore
 
     requestProducts()
+    productSearching()
+
   }
 
   private fun requestProducts() {
@@ -68,9 +73,30 @@ class ShopFragment : Fragment() {
   }
 
   private fun setSelectedProduct(product: Product) {
-    val intent = Intent(requireActivity(), ShoppingCartActivity::class.java).apply {
-      putExtra(ShoppingCartActivity.PRODUCT, product)
+    val intent = Intent(requireActivity(), CartActivity::class.java).apply {
+      putExtra(CartActivity.PRODUCT, product)
     }
     startActivity(intent)
+  }
+
+  private fun productSearching() {
+    binding.searchViewShop.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+      override fun onQueryTextSubmit(query: String?): Boolean {
+        if (query != null) {
+          if (query.isNotEmpty()) {
+            Log.d("SEARCH_QUERY", query)
+            products.clear()
+            adapter.filter.filter(query)
+            populateProducts()
+          }
+        }
+        return true
+      }
+
+      override fun onQueryTextChange(newText: String): Boolean {
+        adapter.filter.filter(newText)
+        return false
+      }
+    })
   }
 }
