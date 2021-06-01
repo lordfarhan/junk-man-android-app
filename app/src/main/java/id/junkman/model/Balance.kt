d8a675255ae3231730b97279fc8819e5b51985f2
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2021-06-01 15:03:48 +0700

    Finish transaction histories

---
+++ app/src/main/java/id/junkman/model/Balance.kt
@@ -0,0 +1,12 @@
+package id.junkman.model
+
+/**
+ * @author farhan
+ * created at at 14:42 on 01/06/21.
+ */
+data class Balance(
+  var id: String = "",
+  var amount: Double = 0.0,
+  var description: String = "",
+  var timestamp: String = ""
+)
---
+++ app/src/main/java/id/junkman/ui/profile/historytrans/HistoryTransAdapter.kt
@@ -0,0 +1,57 @@
+package id.junkman.ui.profile.historytrans
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import id.junkman.App
+import id.junkman.R
+import id.junkman.databinding.ItemHistoryTransBinding
+import id.junkman.model.Balance
+import kotlin.math.abs
+
+/**
+ * @author farhan
+ * created at at 14:42 on 01/06/21.
+ */
+class HistoryTransAdapter : ListAdapter<Balance, HistoryTransAdapter.ViewHolder>(DiffCallback()) {
+  class ViewHolder(private val binding: ItemHistoryTransBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    fun bind(balance: Balance) {
+      binding.transRp.text = String.format("Rp%.0f", abs(balance.amount))
+      if (balance.amount > 0) {
+        binding.txtTypeTrans.text = App.getContext().resources.getString(R.string.type_trans_income)
+      } else {
+        binding.txtTypeTrans.text =
+          App.getContext().resources.getString(R.string.type_trans_outcome)
+      }
+      binding.txtDateTrans.text = balance.timestamp
+    }
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    return ViewHolder(
+      ItemHistoryTransBinding.inflate(
+        LayoutInflater.from(parent.context),
+        parent,
+        false
+      )
+    )
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    holder.bind(getItem(position))
+  }
+}
+
+class DiffCallback() : DiffUtil.ItemCallback<Balance>() {
+  override fun areItemsTheSame(oldItem: Balance, newItem: Balance): Boolean {
+    return oldItem == newItem
+  }
+
+  override fun areContentsTheSame(oldItem: Balance, newItem: Balance): Boolean {
+    return oldItem.id == newItem.id
+  }
+
+}
