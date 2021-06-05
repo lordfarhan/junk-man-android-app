642b715cbd564deabf979359cd862333cb9b400f
Muhammad Farhan Abdur Rosyid <farhan1205muhammad@gmail.com>
2021-06-05 11:34:29 +0700

    Fix withdrawal

--- app/src/main/java/id/junkman/ui/profile/withdraw/BottomWithdrawFragment.kt
+++ app/src/main/java/id/junkman/ui/profile/withdraw/BottomWithdrawFragment.kt
@@ -1,30 +1,67 @@
 package id.junkman.ui.profile.withdraw
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import id.junkman.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import id.junkman.databinding.FragmentBottomWithdrawBinding
 
-class BottomWithdrawFragment : BottomSheetDialogFragment() {
+class BottomWithdrawFragment(
+  private val mContext: Context,
+  private val data: HashMap<String, Any>,
+  private val amount: Int,
+  private val adminFee: Int
+) :
+  BottomSheetDialogFragment() {
+  private lateinit var auth: FirebaseAuth
+  private lateinit var store: FirebaseFirestore
   private var _binding: FragmentBottomWithdrawBinding? = null
   private val binding get() = _binding!!
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
     _binding = FragmentBottomWithdrawBinding.inflate(inflater, container, false)
     return binding.root
   }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    auth = Firebase.auth
+    store = Firebase.firestore
+
+    val user = auth.currentUser!!
+    val total = amount + adminFee
+
+    binding.apply {
+      txtAmountWithdraw.text = String.format("Rp%d", amount)
+      txtAmountAdmin.text = String.format("Rp%d", adminFee)
+      txtAmountTotal.text = String.format("Rp%d", total)
+    }
 
     binding.btnConfirmWithdraw.setOnClickListener {
       //status berubah jadi BATAL
+      store.collection("Withdrawals").add(data)
+        .addOnSuccessListener {
+          Toast.makeText(
+            mContext,
+            "Berhasil mengajukan, mohon tunggu informasi selanjutnya!",
+            Toast.LENGTH_SHORT
+          ).show()
+        }
+        .addOnFailureListener {
+          Toast.makeText(mContext, "Upps, ada yang eror nih!", Toast.LENGTH_SHORT).show()
+        }
       Toast.makeText(requireContext(), "Penarikan Diproses", Toast.LENGTH_SHORT).show()
       dismiss()
     }
--- app/src/main/java/id/junkman/ui/profile/withdraw/WithdrawActivity.kt
+++ app/src/main/java/id/junkman/ui/profile/withdraw/WithdrawActivity.kt
@@ -1,8 +1,15 @@
 package id.junkman.ui.profile.withdraw
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 //import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 /*import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -10,33 +17,108 @@ import com.google.android.material.button.MaterialButton
 import com.google.android.material.textview.MaterialTextView*/
 import id.junkman.R
 import id.junkman.databinding.ActivityWithdrawBinding
-
+import id.junkman.model.Balance
 
 class WithdrawActivity : AppCompatActivity() {
   private lateinit var binding: ActivityWithdrawBinding
+  private lateinit var auth: FirebaseAuth
+  private lateinit var store: FirebaseFirestore
+
+  private var balance = 0.0
+  private var selectedAmount = 0
+  private var adminFee = 0
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     binding = ActivityWithdrawBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
+    auth = Firebase.auth
+    store = Firebase.firestore
+
+    val user = auth.currentUser!!
+
     setActionBar()
 
-    binding.btnSend.setOnClickListener {
-      showBottomDialog()
+    store.collection("Users").document(user.uid)
+      .collection("Balance")
+      .get()
+      .addOnSuccessListener { documents ->
+        for (document in documents) {
+          if (document.exists()) {
+            val bal: Balance = document.toObject(Balance::class.java)
+            balance += bal.amount
+          }
+        }
+        binding.txtSaldo.text = String.format("Rp%.0f", balance)
+      }
+
+    binding.themedToggleBtnGroupWithdrawOption.setOnSelectListener {
+      when (it.id) {
+        R.id.themedBtd50k -> {
+          selectedAmount = 50000
+          adminFee = 5000
+        }
+        R.id.themedBtd100k -> {
+          selectedAmount = 100000
+          adminFee = 4000
+        }
+        R.id.themedBtd200k -> {
+          selectedAmount = 200000
+          adminFee = 2500
+        }
+        else -> {
+          selectedAmount = 0
+          adminFee = 0
+        }
+      }
+      binding.txtAdminFee.setText(adminFee.toString())
     }
 
+    binding.btnSend.setOnClickListener {
+//      showBottomDialog()
+      val bankName = binding.txtBankName.text.toString().trim()
+      val accountName = binding.txtAccountName.text.toString().trim()
+      val accountNumber = binding.txtAccountNumber.text.toString().trim()
+
+      if (selectedAmount != 0 && bankName.isNotEmpty() && accountName.isNotEmpty() && accountNumber.isNotEmpty()) {
+        if (selectedAmount.toDouble() + adminFee.toDouble() > balance) {
+          Toast.makeText(this, "Saldo anda tidak mencukupi!", Toast.LENGTH_SHORT).show()
+        } else {
+          val data: HashMap<String, Any> = hashMapOf(
+            "userId" to user.uid,
+            "bankName" to bankName,
+            "accountName" to accountName,
+            "accountNumber" to accountNumber,
+            "amount" to selectedAmount,
+            "adminFee" to adminFee,
+            "status" to "waiting",
+          )
+
+          showBottomDialog(data)
+        }
+      } else {
+        Toast.makeText(this, "Mohon melengkapi form.", Toast.LENGTH_SHORT).show()
+      }
+    }
   }
 
   private fun setActionBar() {
     supportActionBar?.apply {
-//      setDisplayHomeAsUpEnabled(true)
+      setDisplayHomeAsUpEnabled(true)
       title = resources.getString(R.string.withdraw)
     }
   }
 
-  private fun showBottomDialog() {
-    val bottomWithdrawFragment = BottomWithdrawFragment()
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      android.R.id.home -> onBackPressed()
+    }
+    return super.onOptionsItemSelected(item)
+  }
+
+  private fun showBottomDialog(data: HashMap<String, Any>) {
+    val bottomWithdrawFragment = BottomWithdrawFragment(this, data, selectedAmount, adminFee)
     bottomWithdrawFragment.show(supportFragmentManager, "withdraw")
   }
 
